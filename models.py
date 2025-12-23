7ab5bff9954038d96360ebc66fd443f0f7131f7f
pedro_pereira <pedro.pereira@jscrambler.com>
2025-12-23 19:15:58 +0000

    fix bug on lstm grid search

--- models.py
+++ models.py
@@ -1177,7 +1177,7 @@ def lstm_grid_search_cv(
     results_df = results_df.sort_values('avg_rmse', ascending=True).reset_index(drop=True)
     
     if verbose >= 1:
-        print_grid_search_results(results_df, best_overall_score, best_overall_results, best_avg_params, best_avg_score, best_avg_results, best_avg_params, best_last_fold_score, best_last_fold_results, best_avg_score)
+        print_grid_search_results(results_df, best_overall_score, best_overall_results, best_overall_params, best_avg_score, best_avg_results, best_avg_params, best_last_fold_score, best_last_fold_results, best_last_fold_params)
     
     return {
         'results': results_df,
